7db684674e497bbed88a3102d652d939592b98fc
ronggurmahendra <ronggur.mwp@gmail.com>
2021-03-25 21:01:08 +0700

    adding compile .bat

---
+++ compileProject.bat
@@ -0,0 +1,6 @@
+
+
+./src/class/
+g++ -o main ./src/class/GameState/mainState.cpp ./src/class/GameState/GameState.cpp ./src/class/ShortInput/ShortInputWindows.cpp ./src/class/Engimon/Engimon.cpp ./src/class/Battle/Battle.cpp ./src/class/Map/Map.cpp ./src/class/Skill/Skill.cpp ./src/class/Elements/Elements.cpp ./src/class/Skill_Item/Skill_Item.cpp ./src/class/Player/Player.cpp ./src/class/Breed/Breeding.cpp ./src/class/Exception/Exception.cpp
+
+pause
